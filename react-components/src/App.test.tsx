06957fbffcdbdcfe3849581c3e2ccc970ef60abd
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-03-31 00:03:09 +0300

    feat: add tests for components: App, Header, SearchBar, UserCard

--- react-components/src/App.test.tsx
+++ react-components/src/App.test.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import ROUTES from './constants/routes';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/MainPage/i);
-  expect(linkElement).toBeInTheDocument();
-  screen.debug();
+describe('App', () => {
+  test('go to AboutUsPage from MainPage', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTES.MAIN]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const searchBarElement = screen.getByRole('searchbox');
+    expect(searchBarElement).toBeInTheDocument();
+
+    const aboutUsLinkElement = screen.getByText(/about us/i);
+    aboutUsLinkElement.click();
+
+    const aboutUsPageElement = screen.getByText(/AboutUsPage/i);
+    expect(aboutUsPageElement).toBeInTheDocument();
+  });
+
+  test('go to MainPage from AboutUsPage', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTES.ABOUT_US]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const aboutUsPageElement = screen.getByText(/AboutUsPage/i);
+    expect(aboutUsPageElement).toBeInTheDocument();
+
+    const mainLinkElement = screen.getByText(/main/i);
+    mainLinkElement.click();
+
+    const searchBarElement = screen.getByRole('searchbox');
+    expect(searchBarElement).toBeInTheDocument();
+  });
+
+  test('NotFoundPage is rendered', () => {
+    render(
+      <MemoryRouter initialEntries={['/random-word']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const errorNumber = screen.getByText('404');
+    const errorText = screen.getByText('Page not found');
+
+    expect(errorNumber).toBeInTheDocument();
+    expect(errorText).toBeInTheDocument();
+  });
 });
